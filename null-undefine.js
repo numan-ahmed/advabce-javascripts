4d54d79a555793768b32d1b1db3e73510ded4ba6
numan-ahmed <ahmednuman6826@gmail.com>
2021-02-02 15:47:33 +0600

    null vs undefined cleared for intervies

---
+++ null-undefine.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1, num2){
+
+    return num1 + num2;
+}
+function add(num1,num2){
+    return  num1 + num2;
+}
+const premik = {name:"smart dude",phone:'999348948'};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let age = [10, 12,89];
+console.log(age[11]);
+
