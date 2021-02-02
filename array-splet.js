d95feb00fe4fed43e3fc46340c19d5338abf1613
numan-ahmed <ahmednuman6826@gmail.com>
2021-02-02 18:59:35 +0600

    array split kucha kichi koralm

---
+++ array-splet.js
@@ -0,0 +1,13 @@
+const numbers =  [1,2,3,4,5,6,7,8];
+
+// const part = numbers.slice(2,5);
+
+
+// const removed = numbers.splice(2,3,44,55,77,111,22,33);
+
+// console.log(removed);
+
+console.log(numbers);
+
+const together = numbers.join(" ami tumay valo bashi ");
+console.log(together);
