62c77fa106b5c87e4e5e9f8eeacc21c98f0e6e00
numan-ahmed <ahmednuman6826@gmail.com>
2021-02-02 15:24:04 +0600

    if else statement practice

---
+++ truefalse.js
@@ -0,0 +1,22 @@
+// Falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+//false
+
+// Turthy
+// '0',  " ", [],{},
+
+
+let name =false;
+if(name){
+    console.log('condition is true')
+
+}
+else{
+    console.log('condition is false')
+
+
+}
